0017a86a9f40bd3af390fc0edd539776f7ac9e38
Hammy Goonan <hammy@Hammy-Goonans-MacBook-Pro.local>
2011-02-18 01:25:34 +0000

    added connection between upper and lower house electorates but Electorates don't seem to be saving

--- app/models/electorate.php
+++ app/models/electorate.php
@@ -2,6 +2,14 @@
 	class Electorate extends AppModel {
 		var $name = 'Electorate';
 		var $hasMany = array('Members');
-		var $hasAndBelongsToMany = array('Pcode');
+		var $hasAndBelongsToMany = array('Pcode',
+			'Electorate' => 
+				array(
+					'className' => 'Electorate',
+					'joinTable' => 'lower_upper',
+					'foreignKey' => 'lower_id',
+					'associationForeignKey'  => 'upper_id'
+				)
+			);
 	}
 ?>
