daf02a6643168395d08d4005c42bae6182500d82
Hammy Goonan <hammy@spiresoftware.com.au>
2014-03-01 19:22:18 +0000

    DJANGO DJANGO DJANGO

--- electorate.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-class Electorate:
-	def __init__(self, data):
-		self.id = data[0]
-		self.name = data[1]
-		self.duristriction = data[2]
-	def add_member(self, member):
-		pass
