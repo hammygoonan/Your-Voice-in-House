18db6289f4e63dc7681e84f7ecf5d9b6c34e2167
Hammy Goonan <hammy@spiresoftware.com.au>
2015-04-18 11:16:23 +0100

    improvements to QLD scraper

--- data/qld.py
+++ data/qld.py
@@ -44,15 +44,31 @@ class QldData(BaseData):
         electorate = self.getElectorate(electorate_name, 7)
         party = self.getParty(bio_p[1].text.replace('Party', '').strip())
         job_list = bio.find('ul', 'listnoindent')
-        job = job_list.text.split(':')[0]
-        
+        role = job_list.text.split(':')[0]
+
+        # images
+        img_src = bio.find('img')
+        img_src = img_src['src'].replace('../../../', 'https://www.parliament.qld.gov.au/')
+        photo = self.saveImg(img_src, first_name + '_' + second_name + '.jpg', 'act')
+
+        # create member
+        member = models.Member(first_name, second_name, role, electorate, party, photo)
+        db.session.add(member)
+
+        # emails and links
+        for link in bio.find_all('a'):
+            if link['href'].find('mailto:') > -1:
+                email = models.Email(link['href'].replace('mailto:', ''), member)
+                db.session.add(email)
+            elif link['href'].find('javascript:DoSpeechSearch()') < 0 and link['href'].find('.pdf') < 0:
+                new_link = models.Link(link['href'], 'website', member)
+                db.session.add(new_link)
+        print re.findall(ur'\([0-9]*\)\ [0-9]*\ [0-9]*', bio.text)
+
+
+
 
         '''
-        role
-        photo
-
-        email
         address
         phone_numbers
-        links
         '''
